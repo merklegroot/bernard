a2b56daa4f520717cc3b4550212af9b6d4343fa4
Merkle Groot <merklegroot@gmail.com>
2025-01-30 15:12:36 -0500

    showing the exits in the room

--- src/Game/controls/room_exits_panel/RoomExitsPanel.cs
+++ src/Game/controls/room_exits_panel/RoomExitsPanel.cs
@@ -1,8 +1,13 @@
 using Godot;
+using System.Collections.Generic;
+using Game.Models;
+using Game.Repo;
 
 public partial class RoomExitsPanel : Panel
 {
     private HFlowContainer _itemContainer;
+
+    private readonly RoomDefRepo _roomDefRepo = new();
 
     public override void _Ready()
     {
@@ -19,13 +24,35 @@ public partial class RoomExitsPanel : Panel
 
     private void UpdateDisplay()
     {
+        var roomDef = _roomDefRepo.Get(GameStateContainer.GameState.RoomId);
+
+        UpdateExits(roomDef.Exits);
+    }
+
+    private void UpdateExits(List<RoomExit> exits)
+    {
         foreach (var child in _itemContainer.GetChildren())
         {
             child.QueueFree();
         }
 
-        // Temporary hardcoded exits
-        var label = new Label { Text = "None" };
-        _itemContainer.AddChild(label);
+        if (exits.Count == 0)
+        {
+            var label = new Label { Text = "None" };
+            _itemContainer.AddChild(label);
+            return;
+        }
+
+        foreach (var exit in exits)
+        {
+            var button = new Button
+            {
+                Text = exit.Direction.ToString(),
+                CustomMinimumSize = new Vector2(100, 30),
+                SizeFlagsHorizontal = SizeFlags.ShrinkCenter
+            };
+            
+            _itemContainer.AddChild(button);
+        }
     }
 } 
