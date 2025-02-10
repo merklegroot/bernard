c73668a2088eeb64c3bad5f9a6f23640e63cdab2
Merkle Groot <merklegroot@gmail.com>
2025-02-10 08:57:26 -0500

    make it possible to close the fight panel

--- src/Game/controls/fight_panel/FightPanel.cs
+++ src/Game/controls/fight_panel/FightPanel.cs
@@ -1,8 +1,25 @@
 using Godot;
+using Game.Events;
+using Game.Constants;
 
 public partial class FightPanel : Panel
 {
+    private Button _closeButton;
+    
     public override void _Ready()
     {
+        _closeButton = GetNode<Button>("VBoxContainer/HBoxContainer/CloseButton");
+        _closeButton.Pressed += OnCloseButtonPressed;
+    }
+
+    private void OnCloseButtonPressed()
+    {
+        EventBus.Instance.EmitSignal(EventBus.SignalName.SetMainPanel, (int)PanelEnum.Room);
+    }
+
+    public override void _ExitTree()
+    {
+        base._ExitTree();
+        _closeButton.Pressed -= OnCloseButtonPressed;
     }
 } 
